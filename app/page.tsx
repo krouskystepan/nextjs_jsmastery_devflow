a2dd26feddb9d12bb68728e2f0e83321e0067206
Stepan Krousky <stepan.krousky@seznam.cz>
2023-09-28 11:36:03 +0200

    routing and auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next13</h1>
-      <h2 className="h2-bold">Next13</h2>
-      <h3 className="h3-bold">Next13</h3>
-    </div>
-  );
-}
