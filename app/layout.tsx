779df60bd8d733f8c0659c8da1064d7436c8cfc2
Stepan Krousky <stepan.krousky@seznam.cz>
2023-11-03 20:19:54 +0100

    webhook

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import { ClerkProvider } from '@clerk/nextjs'
-// eslint-disable-next-line camelcase
-import { Inter, Space_Grotesk } from 'next/font/google'
-import type { Metadata } from 'next'
+/* eslint-disable camelcase */
+import React from 'react';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type { Metadata } from 'next';
 
-import './globals.css'
-import { ThemeProvider } from '@/context/ThemeProvider'
+import './globals.css';
+import { ThemeProvider } from '@/context/ThemeProvider';
 
 const inter = Inter({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   variable: '--font-inter',
-})
+});
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-spaceGrotesk',
-})
+});
 
 export const metadata: Metadata = {
   title: 'DevFlow',
   description:
-    'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more',
+    'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
   icons: {
-    icon: '/assets/iamges/site-logo.svg',
+    icon: '/assets/images/site-logo.svg',
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -44,11 +44,9 @@ export default function RootLayout({
             },
           }}
         >
-          <ThemeProvider>
-            {children}
-          </ThemeProvider>
+          <ThemeProvider>{children}</ThemeProvider>
         </ClerkProvider>
       </body>
     </html>
-  )
+  );
 }
