bd96499dd4c165e419f520d18173d36b76d003c4
Stepan Krousky <stepan.krousky@seznam.cz>
2023-11-03 22:21:42 +0100

    ...

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -57,7 +57,7 @@ const QuestionCard = ({
 
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          imgUrl={author.picture}
+          imgUrl="/assets/icons/avatar.svg"
           alt="user"
           value={author.name}
           title={` - asked ${getTimestamp(createdAt)}`}
